d87b2ec75fd84aecd94a02417dc0669ed180e03d
jrossonrinehart <jrossonrinehart@gmail.com>
2024-10-21 20:56:08 -0500

    add Player class and instantiate our player in main

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    dt = 0
     #Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="black")
+        player1.draw(screen=screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
     
 
 
